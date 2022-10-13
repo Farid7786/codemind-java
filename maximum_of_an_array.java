9e93045f69fdc1fff85036d526f99ddeb6e1d554
ShaikFarid <99642875+Farid7786@users.noreply.github.com>
2022-10-13 20:26:20 +0530

    import java.util.Scanner; class Even {     public static void main(String args[])     {         int n,i,sum;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         sum=a[0];         for(i=0;i<n;i++)         {             if(a[i]>=sum)             {               sum=a[i];             }         }         System.out.print(sum);         sc.close();     } }

---
+++ maximum_of_an_array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Even
+{
+    public static void main(String args[])
+    {
+        int n,i,sum;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        sum=a[0];
+        for(i=0;i<n;i++)
+        {
+            if(a[i]>=sum)
+            {
+              sum=a[i];
+            }
+        }
+        System.out.print(sum);
+        sc.close();
+    }
+}
