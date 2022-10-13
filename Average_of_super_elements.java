7696092fd3a4e24edd0ed471daa11ee69e9f4f68
ShaikFarid <99642875+Farid7786@users.noreply.github.com>
2022-10-13 19:00:35 +0530

    import java.util.Scanner; class minimum {     public static void main(String args[])     {         int n,i,j,c=0,sum=0,m=0;         float avg;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {            c=0;            for(j=0;j<n;j++)            {                if(a[i]==a[j] && a[i]!=-1)                {                    c++;                }            }            if(c==a[i])            {                sum=sum+a[i];                m++;                a[i]=-1;            }         }         if(m==0)         {             System.out.println("-1");         }         else         {             avg=(float)sum/m;             System.out.format("%.2f",avg);         }         sc.close();     } }

---
+++ Average_of_super_elements.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class minimum
+{
+    public static void main(String args[])
+    {
+        int n,i,j,c=0,sum=0,m=0;
+        float avg;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+           c=0;
+           for(j=0;j<n;j++)
+           {
+               if(a[i]==a[j] && a[i]!=-1)
+               {
+                   c++;
+               }
+           }
+           if(c==a[i])
+           {
+               sum=sum+a[i];
+               m++;
+               a[i]=-1;
+           }
+        }
+        if(m==0)
+        {
+            System.out.println("-1");
+        }
+        else
+        {
+            avg=(float)sum/m;
+            System.out.format("%.2f",avg);
+        }
+        sc.close();
+    }
+}
