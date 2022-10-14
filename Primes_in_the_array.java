da2966cae2622bb299e9366ed27c6a9c8b651b27
ShaikFarid <99642875+Farid7786@users.noreply.github.com>
2022-10-14 21:01:20 +0530

    import java.util.Scanner; class Descending {     public static void main(String args[])     {         int n,i,j,c=0,m=0,k,s;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             c=0;             k=a[i];             for(j=1;j<=k;j++)             {                 if(k%j==0)                 {                     c++;                 }             }             if(c==2)             {                 m++;             }         }         System.out.print(m);     } }

---
+++ Primes_in_the_array.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Descending
+{
+    public static void main(String args[])
+    {
+        int n,i,j,c=0,m=0,k,s;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            c=0;
+            k=a[i];
+            for(j=1;j<=k;j++)
+            {
+                if(k%j==0)
+                {
+                    c++;
+                }
+            }
+            if(c==2)
+            {
+                m++;
+            }
+        }
+        System.out.print(m);
+    }
+}
