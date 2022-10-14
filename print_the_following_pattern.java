dfbc035b53794c746a487d0a6ff6a23c7be52bcb
ShaikFarid <99642875+Farid7786@users.noreply.github.com>
2022-10-14 22:38:50 +0530

    import java.util.Scanner; import java.lang.Math; class Seasons {     public static void main(String args[])     {         int n,i,j,k;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         for(i=1;i<=n;i++)         {             for(j=1;j<=i;j++)             {                System.out.print(j);             }             System.out.println();         }         sc.close();     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -9,9 +9,9 @@ class Seasons
         n=sc.nextInt();
         for(i=1;i<=n;i++)
         {
-            for(j=1;j<=n;j++)
+            for(j=1;j<=i;j++)
             {
-                System.out.print((char)(64+i)+" ");
+               System.out.print(j);  
             }
             System.out.println();
         }
