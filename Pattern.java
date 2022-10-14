d5a7a6fe0def263dac96d11cc62114cb40045e5b
ShaikFarid <99642875+Farid7786@users.noreply.github.com>
2022-10-14 22:42:48 +0530

    import java.util.Scanner; import java.lang.Math; class Seasons {     public static void main(String args[])     {         int n,i,j,k;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         for(i=1;i<=n;i++)         {             for(j=1;j<=n;j++)             {                System.out.print(i+" ");             }             System.out.println();         }         sc.close();     } }

---
+++ Pattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.Math;
+class Seasons
+{
+    public static void main(String args[])
+    {
+        int n,i,j,k;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+               System.out.print(i+" ");  
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
