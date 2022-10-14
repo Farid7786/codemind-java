c9a1fead49c0fecf542706272aeba215a9969f79
ShaikFarid <99642875+Farid7786@users.noreply.github.com>
2022-10-14 20:50:13 +0530

    import java.util.Scanner; class Descending {     public static void main(String args[])     {         int n,i,c=0,k=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(a[i]>k)             {                 k=a[i];                 c++;             }         }         if(c==n)         {             System.out.println("yes");         }         else         {             System.out.println("no");         }     } }

---
+++ Ascending_array.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Descending
+{
+    public static void main(String args[])
+    {
+        int n,i,c=0,k=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(a[i]>k)
+            {
+                k=a[i];
+                c++;
+            }
+        }
+        if(c==n)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+}
