2ad05f5644c29db0e8853b9e380aefe84e9cdbf2
ShaikFarid <99642875+Farid7786@users.noreply.github.com>
2022-10-15 14:59:51 +0530

    import java.util.Scanner; class Even {     public static void main(String args[])     {         int n,i,j,sum=0,m;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         m=sc.nextInt();         int[][] a=new int[n][m];         for(i=0;i<n;i++)         {             for(j=0;j<m;j++)             {               a[i][j]=sc.nextInt();             }         }         for(i=0;i<n;i++)         {             for(j=0;j<m;j++)             {                 sum=sum+a[i][j];             }         }         System.out.print(sum);         sc.close();     } }

---
+++ Sum_of_matrix_elements.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Even
+{
+    public static void main(String args[])
+    {
+        int n,i,j,sum=0,m;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        m=sc.nextInt();
+        int[][] a=new int[n][m];
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+              a[i][j]=sc.nextInt();   
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                sum=sum+a[i][j];
+            }
+        }
+        System.out.print(sum);
+        sc.close();
+    }
+}
