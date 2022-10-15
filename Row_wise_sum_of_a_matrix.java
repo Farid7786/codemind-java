1e75a9057b01e9bec9415b8a0f3ebbf385eb3902
ShaikFarid <99642875+Farid7786@users.noreply.github.com>
2022-10-15 14:04:21 +0530

    import java.util.Scanner; class Even {     public static void main(String args[])     {         int n,i,j,sum=0,m;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         m=sc.nextInt();         int[][] a=new int[n][m];         for(i=0;i<n;i++)         {            for(j=0;j<m;j++)            {                a[i][j]=sc.nextInt();            }         }         for(i=0;i<n;i++)         {             sum=0;             for(j=0;j<m;j++)             {                 sum=sum+a[i][j];             }             System.out.print(sum+" ");         }         sc.close();     } }

--- Row_wise_sum_of_a_matrix.java
+++ Row_wise_sum_of_a_matrix.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
-class Matrix
+class Even
 {
     public static void main(String args[])
     {
-        int n,m,i,j,sum=0,p=0;
+        int n,i,j,sum=0,m;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
         m=sc.nextInt();
         int[][] a=new int[n][m];
         for(i=0;i<n;i++)
         {
-            for(j=0;j<m;j++)
-            {
-                a[i][j]=sc.nextInt();
-            }
+           for(j=0;j<m;j++)
+           {
+               a[i][j]=sc.nextInt();
+           }
         }
         for(i=0;i<n;i++)
         {
