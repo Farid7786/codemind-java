0d0654638ca69f6c1c6429b01e2ebf7ecd7f5314
ShaikFarid <99642875+Farid7786@users.noreply.github.com>
2022-10-15 14:03:09 +0530

    import java.util.Scanner; class Even {     public static void main(String args[])     {         int n,i,j,sum=0,m;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         m=sc.nextInt();         int[][] a=new int[n][m];         for(i=0;i<n;i++)         {            for(j=0;j<m;j++)            {                a[i][j]=sc.nextInt();            }         }         for(i=0;i<n;i++)         {             for(j=0;j<m;j++)             {                 if(i%2==0)                 {                     sum=sum+a[i][j];                 }             }         }         System.out.print(sum+" ");         sum=0;         for(i=0;i<n;i++)         {             for(j=0;j<m;j++)             {                 if(i%2!=0)                 {                     sum=sum+a[i][j];                 }             }         }         System.out.print(sum);         sc.close();     } }

--- Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.java
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.java
@@ -1,38 +1,43 @@
 import java.util.Scanner;
-class Matrix
+class Even
 {
     public static void main(String args[])
     {
-        int n,m,i,j,sum=0,p=0;
+        int n,i,j,sum=0,m;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
         m=sc.nextInt();
         int[][] a=new int[n][m];
         for(i=0;i<n;i++)
         {
-            for(j=0;j<m;j++)
-            {
-                a[i][j]=sc.nextInt();
-            }
+           for(j=0;j<m;j++)
+           {
+               a[i][j]=sc.nextInt();
+           }
         }
         for(i=0;i<n;i++)
         {
-            if(i%2==0)
+            for(j=0;j<m;j++)
             {
-               for(j=0;j<m;j++)
-               {
-                   sum=sum+a[i][j]; 
-               }
-            }
-            else
-            {
-                for(j=0;j<m;j++)
+                if(i%2==0)
                 {
-                    p=p+a[i][j];
+                    sum=sum+a[i][j];
                 }
             }
         }
-        System.out.print(sum+" "+p);
+        System.out.print(sum+" ");
+        sum=0;
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(i%2!=0)
+                {
+                    sum=sum+a[i][j];
+                }
+            }
+        }
+        System.out.print(sum);
         sc.close();
     }
 }
