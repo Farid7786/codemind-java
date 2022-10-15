90c373e2e5e64630a35d57e29f2990fdd40f1a28
ShaikFarid <99642875+Farid7786@users.noreply.github.com>
2022-10-15 14:09:04 +0530

    import java.util.Scanner; class Even {     public static void main(String args[])     {         int n,i,j,sum=0,m,c=0,k=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         m=sc.nextInt();         int[][] a=new int[n][m];         for(i=0;i<n;i++)         {            for(j=0;j<m;j++)            {                a[i][j]=sc.nextInt();            }         }         for(i=0;i<n;i++)         {             sum=0;             for(j=0;j<m;j++)             {                 sum=sum+a[i][j];             }             if(c<=sum)             {                 c=sum;             }         }         System.out.print(c);         sc.close();     } }

--- Maximum_sum_of_row_wise_sum_of_a_matrix.java
+++ Maximum_sum_of_row_wise_sum_of_a_matrix.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
-class Matrix
+class Even
 {
     public static void main(String args[])
     {
-        int n,m,i,j,sum=0,c=0;
+        int n,i,j,sum=0,m,c=0,k=0;
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
         m=sc.nextInt();
         int[][] a=new int[n][m];
         for(i=0;i<n;i++)
         {
-            for(j=0;j<m;j++)
-            {
-                a[i][j]=sc.nextInt();
-            }
+           for(j=0;j<m;j++)
+           {
+               a[i][j]=sc.nextInt();
+           }
         }
         for(i=0;i<n;i++)
         {
@@ -22,7 +22,7 @@ class Matrix
             {
                 sum=sum+a[i][j];
             }
-            if(sum>c)
+            if(c<=sum)
             {
                 c=sum;
             }
